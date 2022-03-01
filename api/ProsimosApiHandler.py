f3fd8029dfbe68d7cde42c682d060e9ee91d44e5
Iryna Halenok <iryna.halenok@gmail.com>
2022-03-01 13:09:29 +0200

    Add Prosimos git submodule

--- api/ProsimosApiHandler.py
+++ api/ProsimosApiHandler.py
@@ -1,5 +1,14 @@
+from flask import request, jsonify
 from flask_restful import Resource
+from bpdfr_simulation_engine.simulation_engine import run_simulation
 
 class ProsimosApiHandler(Resource):
-  def get(self):
-    return "prosimos get"
+  def post(self):
+    content = request.json
+    totalCases = content['totalCases']
+    startingAt = content['startingAt']
+
+    res = run_simulation("", "", 10)
+
+    # TODO: call simulation here and return the result
+    return jsonify({ "start": res, "t": totalCases })
