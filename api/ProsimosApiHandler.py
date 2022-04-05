2f3c2338fd0f1f0ceabf009dd8edefc270f74ed8
Iryna Halenok <iryna.halenok@gmail.com>
2022-04-06 00:56:29 +0300

    Use the passed from UI json instead of the hardcoded one

--- api/ProsimosApiHandler.py
+++ api/ProsimosApiHandler.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 from io import StringIO
 from flask import request, make_response
@@ -13,52 +14,60 @@ class ProsimosApiHandler(Resource):
     return df_out_json
 
   def post(self):
-    content = request.json
-    numProcesses = content['numProcesses']
-    startDate = content['startDate']
-    jsonData = content['jsonData']
-    xmlData = content['xmlData']
+    try:
+      content = request.json
+      numProcesses = content['numProcesses']
+      startDate = content['startDate']
+      jsonData = content['jsonData']
+      xmlData = content['xmlData']
 
-    json_path = "./temp_files/bimp_example.json" 
-    # json_file = tfile.NamedTemporaryFile(mode="w+", suffix=".json", prefix="params_", delete=False, dir='/tmp')
-    bpmn_file = tfile.NamedTemporaryFile(mode="w+", suffix=".bpmn", prefix="bpmn_model_", delete=False, dir='/tmp')
-    stats_file = tfile.NamedTemporaryFile(mode="w+", suffix=".csv", prefix="stats_", delete=False, dir='/tmp')
-    logs_file = tfile.NamedTemporaryFile(mode="w+", suffix=".csv", prefix="logs_", delete=False, dir='/tmp')
-    logs_filename = logs_file.name.rsplit('/', 1)[-1]
+      # json_path = "./temp_files/bimp_example.json" 
+      json_file = tfile.NamedTemporaryFile(mode="w+", suffix=".json", prefix="params_", delete=False, dir='/tmp')
+      bpmn_file = tfile.NamedTemporaryFile(mode="w+", suffix=".bpmn", prefix="bpmn_model_", delete=False, dir='/tmp')
+      stats_file = tfile.NamedTemporaryFile(mode="w+", suffix=".csv", prefix="stats_", delete=False, dir='/tmp')
+      logs_file = tfile.NamedTemporaryFile(mode="w+", suffix=".csv", prefix="logs_", delete=False, dir='/tmp')
+      logs_filename = logs_file.name.rsplit('/', 1)[-1]
 
-    # TODO: uncomment when new json format will be handled correctly
-    # with json_file as file_writter:
-    #   json.dump(jsonData, file_writter)
-    
-    with bpmn_file as f:
-      f.write(xmlData)
+      with json_file as file_writter:
+        json.dump(jsonData, file_writter)
+      
+      with bpmn_file as f:
+        f.write(xmlData)
 
-    date = datetime.strptime(startDate, "%Y-%m-%dT%H:%M:%S.%f%z")
+      date = datetime.strptime(startDate, "%Y-%m-%dT%H:%M:%S.%f%z")
 
-    # run simulation
-    _ = run_simulation(bpmn_file.name, json_path,
-      total_cases=numProcesses,
-      stat_out_path=stats_file.name,
-      log_out_path=logs_file.name,
-      starting_at=date)
+      # run simulation
+      _ = run_simulation(bpmn_file.name, json_file.name,
+        total_cases=numProcesses,
+        stat_out_path=stats_file.name,
+        log_out_path=logs_file.name,
+        starting_at=date)
 
-    with stats_file as f:
-      contents = f.read()
+      with stats_file as f:
+        contents = f.read()
 
-    _, out2, out3, out4 = contents.split('\n""\n')
+      _, out2, out3, out4 = contents.split('\n""\n')
 
-    df_out2_json = self.__getJsonString(out2)
-    df_out3_json = self.__getJsonString(out3)
-    df_out4_json = self.__getJsonString(out4)
+      df_out2_json = self.__getJsonString(out2)
+      df_out3_json = self.__getJsonString(out3)
+      df_out4_json = self.__getJsonString(out4)
 
-    str = f"""{{
-            "Resource Utilization": {df_out2_json},
-            "Individual Task Statistics": {df_out3_json},
-            "Overall Scenario Statistics": {df_out4_json},
-            "LogFileName": "{logs_filename}"
-          }}"""
+      str = f"""{{
+              "Resource Utilization": {df_out2_json},
+              "Individual Task Statistics": {df_out3_json},
+              "Overall Scenario Statistics": {df_out4_json},
+              "LogFileName": "{logs_filename}"
+            }}"""
 
-    response = make_response(str)
-    response.headers['content-type'] = 'application/json'
+      response = make_response(str)
+      response.headers['content-type'] = 'application/json'
+      return response
 
-    return response
+    except Exception as e:
+      print(e)
+      response = {
+        "displayMessage": "Something went wrong",
+        # "errorDesciption": jsonify(e)
+      }
+
+      return response, 500
